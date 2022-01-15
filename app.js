191be8d85be904a1176568dc59c68a3c01ca9620
Akshay <akshaydevadiga4@gmail.com>
2022-01-15 20:01:08 +0530

    Implement sql functions,support pagination and add bearer token implementation

--- app.js
+++ app.js
@@ -1,66 +1,96 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
+const mysql = require('mysql');
+const VerificationHelper = require('./verificationHelper')
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'acedemiatest'
 
-const users = [{"id":"Universidad Católica Popular del Risaralda","email":"jmacascaidh0@blinklist.com","gender":"Male","profilepic":"https://robohash.org/asperioressuscipitblanditiis.png?size=50x50&set=set1"},
-{"id":"State Film, Television and Theatre Higher School Leon Schiller in Lodz","email":"dgrabiec1@merriam-webster.com","gender":"Male","profilepic":"https://robohash.org/adipiscidoloresimpedit.png?size=50x50&set=set1"},
-{"id":"Bauder College","email":"jroly2@chronoengine.com","gender":"Female","profilepic":"https://robohash.org/iuresitnihil.png?size=50x50&set=set1"},
-{"id":"Beth-El College of Nursing and Health Sciences","email":"gkuscha3@walmart.com","gender":"Female","profilepic":"https://robohash.org/doloremeosducimus.png?size=50x50&set=set1"},
-{"id":"Vaasa University of Applied Sciences","email":"kseemmonds4@marketwatch.com","gender":"Female","profilepic":"https://robohash.org/officiisvelitreprehenderit.png?size=50x50&set=set1"},
-{"id":"Technical University of Koszalin","email":"fmccuaig5@admin.ch","gender":"Male","profilepic":"https://robohash.org/eiusadipisciquo.png?size=50x50&set=set1"},
-{"id":"Agricultural University of Warsaw","email":"glemarquis6@technorati.com","gender":"Male","profilepic":"https://robohash.org/quiadebitisexpedita.png?size=50x50&set=set1"},
-{"id":"Université des Sciences et de la Technologie d'Oran","email":"rrain7@posterous.com","gender":"Male","profilepic":"https://robohash.org/aspernaturnonsequi.png?size=50x50&set=set1"},
-{"id":"Celal Bayar University","email":"obrugsma8@geocities.jp","gender":"Female","profilepic":"https://robohash.org/hicsittempora.png?size=50x50&set=set1"},
-{"id":"Perm State Pedagogical University","email":"rwestbrook9@alibaba.com","gender":"Female","profilepic":"https://robohash.org/eaundesint.png?size=50x50&set=set1"},
-{"id":"Dares Salaam Institute of Technology","email":"chessela@163.com","gender":"Female","profilepic":"https://robohash.org/eaqueuteum.png?size=50x50&set=set1"},
-{"id":"McKendree College","email":"amarteb@usgs.gov","gender":"Male","profilepic":"https://robohash.org/aliquidcumperspiciatis.png?size=50x50&set=set1"},
-{"id":"Fachhochschule Schmalkalden","email":"hmacc@pcworld.com","gender":"Female","profilepic":"https://robohash.org/consequaturfugitsit.png?size=50x50&set=set1"},
-{"id":"Barton College","email":"atrottond@privacy.gov.au","gender":"Female","profilepic":"https://robohash.org/aspernaturminimaearum.png?size=50x50&set=set1"},
-{"id":"Hogeschool voor Wetenschap & Kunst","email":"rcozzie@jigsy.com","gender":"Male","profilepic":"https://robohash.org/odiosuntvoluptatem.png?size=50x50&set=set1"},
-{"id":"Nanjing Forestry University","email":"mblaberf@msu.edu","gender":"Female","profilepic":"https://robohash.org/perspiciatisdoloribusvoluptas.png?size=50x50&set=set1"},
-{"id":"Philadelphia University","email":"zlaskeyg@hugedomains.com","gender":"Male","profilepic":"https://robohash.org/voluptatesperspiciatisautem.png?size=50x50&set=set1"},
-{"id":"Universidade Regional de Blumenau","email":"bkinghh@paginegialle.it","gender":"Male","profilepic":"https://robohash.org/magnamseddignissimos.png?size=50x50&set=set1"}]
+});
 
+db.connect((err)=>{
+    if(err){
+        throw err
+    }
+    console.log("mysql is connected")
+});
 
-app.get('/students',paginateResults(users),(req, res)=>{
-    res.send(res.paginatedResults);
-})
-
-app.get('/courses',(req, res)=>{
-
-})
-
-app.get('/users',(req, res)=>{
-
+app.get('/api',(req, res)=>{
+    res.send("API Version 1.0");
 })
 
 
-function paginateResults (model){
-    return (req,res,next) =>{
-        let page = parseInt(req.query.page);
-        let limit = parseInt(req.query.limit);
-        let startIndex = (page - 1) * limit;
-        let endIndex = page * limit;
-        let results = model.slice(startIndex, endIndex);
-        let response = {};
-        response.results=results;
-    
-        if(endIndex<model.length){
-        response.next = {
-            page: page+1,
-            limit: limit
+app.get('/api/students',VerificationHelper.verifyToken,(req, res)=>{
+    jwt.verify(req.token,'secretkey',(err,authData) =>{
+        if(err){
+            console.log(err)
+            res.sendStatus(403)
+        }else{
+            let page = parseInt(req.query.page);
+            let limit = parseInt(req.query.limit);
+            let offset = page ? (page - 1) * limit: 1;
+            limit = limit ? limit : 5;
+
+            let sql = `SELECT * FROM students LIMIT ${offset}, ${limit}`;
+            db.query(sql,(err,result)=>{
+                if(err) throw err;
+                let response = {}
+                response.result = result;
+                if(limit<result.length){
+                    response.next = {
+                        page: page+1,
+                        limit: limit
+                    }
+                }
+                
+                    if(offset>0){
+                    response.prevous = {
+                        page: page-1,
+                        limit: limit
+                    }
+                }
+                res.send(response);
+            });     
         }
+    });
+})
+
+
+app.post('/api/courses',VerificationHelper.verifyToken,(req, res)=>{
+    jwt.verify(req.token,'secretkey',(err,authData) =>{
+        if(err){
+            console.log(err)
+            res.sendStatus(403)
+        }else{
+            let sql = 'SELECT * FROM courses';
+            db.query(sql,(err,result)=>{
+                if(err) throw err;
+                res.send(result);
+            });     
         }
-    
-        if(startIndex>0){
-        response.prevous = {
-            page: page-1,
-            limit: limit
+    });
+})
+
+app.post('/api/login',(req, res)=>{
+    // Validate user with db 
+    // If Yes - return back access token and set this access token in local storage in client
+    // If No - Return 404
+})
+
+app.post('/api/createUser',VerificationHelper.verifyToken,(req, res)=>{
+    jwt.verify(req.token,'secretkey',(err,authData) =>{
+        if(err){
+            console.log(err)
+            res.sendStatus(403)
+        }else{
+            // Get the user object from client
+            // Get Generate access token for the user
+            // Update the db with access token   
         }
-        }
-        
-        res.paginatedResults = response;
-        next()
-    } 
-}
+    });
+})
 
 app.listen(3000)
